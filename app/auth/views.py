06c4d3ae23c35a5746c1fb000d85a9443fd8ee74
Natalie Costa <costa.natalie02@gmail.com>
2020-08-24 19:48:21 -0300

    Adicionando sign up api e generate token

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,12 +1,14 @@
-from flask import make_response, jsonify, request
+from flask import make_response, jsonify, request, Response
 from http import HTTPStatus
 from app import bcrypt
-from models import User
+from models import User, UserSchema
 from flask.views import MethodView
+from app.auth.helpers import generate_token
 from werkzeug.exceptions import BadRequest
 from flask_jwt_extended import create_access_token, get_jwt_identity, create_refresh_token, jwt_required
 from datetime import timedelta
 
+user_schema = UserSchema()
 
 class LoginApi(MethodView):
 	def post(self):
@@ -26,6 +28,7 @@ class LoginApi(MethodView):
 		password = post_data.get("password")
 		
 		obj_users = User.query.filter_by(email=email).first()
+		data, error = user_schema.load(req_data, partial=True) #ver isso aqui 
 
 		if not obj_users:
 			status_code = HTTPStatus.UNAUTHORIZED.value
@@ -36,8 +39,12 @@ class LoginApi(MethodView):
 			status_code = HTTPStatus.UNAUTHORIZED.value
 			return make_response(jsonify(response), status_code)
 
+		if not data.get('email') or not data.get('password'):
+			return custom_response({'error': 'you need email and password to sign in'}, 400)
+		
+
 		js_user = {
-			"name": obj_users.name,
+			"name": obj_users.name, # TODO: fazer um metodo serialize em models.py
 			"email": obj_users.email,
 			"password": obj_users.password,
 			"id": obj_users.id
@@ -65,6 +72,42 @@ class SignUpApi(MethodView):
 	decorators = [jwt_required]
 	def post(self):
 		print("FÉ EM DEUS DJ")
+		
 		response = dict(status="fail")
+		data, error = user_schema.load(response)
 
-		return make_response(jsonify(response), HTTPStatus.OK.value)
+		try:
+			post_data = request.get_json(force=True) 
+		except BadRequest:
+			response.update(dict(message="O dado informado não foi aceito"))
+			status_code = HTTPStatus.BAD_REQUEST.value
+			return make_response(jsonify(response), status_code)
+
+		if error:
+			return custom_response(error, 400)
+		# check if user already exist in the db
+		user_in_db = User.get_user_by_email(data.get('email'))
+
+		if user_in_db:
+			message = {'error': 'User already exist, please supply another email address'}
+			return custom_response(message, 400)
+
+		user = User(data)
+		user.save()
+
+		ser_data = user_schema.dump(user).data
+
+		token = Auth.generate_token(ser_data.get('id'))
+
+		return custom_response({'jwt_token': token}, 201)
+		#return make_response(jsonify(response), HTTPStatus.OK.value)
+
+	def custom_response(res, status_code):
+  		"""
+  		Custom Response Function
+  		"""
+  		return Response(
+    		mimetype="application/json",
+    		response=json.dumps(res),
+    		status=status_code
+  		)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow import fields, Schema
 
 
 class User(db.Model):
@@ -10,8 +11,64 @@ class User(db.Model):
     name = db.Column(db.String(25), nullable=False)
     email = db.Column(db.String(45), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
-    #status_id = db.Column(db.Integer, db.ForeignKey('user_status'))
+    # TODO: status_id = db.Column(db.Integer, db.ForeignKey('user_status'))
+    #created_at = db.Column(db.DateTime)
+    #modified_at = db.Column(db.DateTime)
 
+    def __init__(self, data):
+        """
+        Class constructor
+        """
+        self.name = data.get('name')
+        self.email = data.get('email')
+        self.password = self.__generate_hash(data.get('password'))
+        #self.created_at = datetime.datetime.utcnow()
+        #self.modified_at = datetime.datetime.utcnow()  
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self, data):
+        for key, item in data.items():
+            if key == 'password':
+                self.password = self.__generate_hash(value)
+            setattr(self, key, item)
+        #self.modified_at = datetime.datetime.utcnow()
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all_users():
+        return User.query.all()
+
+    @staticmethod
+    def get_one_user(id):
+        return User.query.get(id)
+  
+    @staticmethod
+    def get_user_by_email(value):
+        return User.query.filter_by(email=value).first()
+
+    def __generate_hash(self, password):
+        return bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
+  
+    def check_hash(self, password):
+        return bcrypt.check_password_hash(self.password, password)
+  
+    def __repr(self):
+        return '<id {}>'.format(self.id)
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    email = fields.Email(required=True)
+    password = fields.Str(required=True, load_only=True)
+    #created_at = fields.DateTime(dump_only=True)
+    #modified_at = fields.DateTime(dump_only=True)
 
 class UserStatus(db.Model):
     """ User Status Model """
