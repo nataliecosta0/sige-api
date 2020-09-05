0506c44c3aeef44faf19fb0791fb09ad94507f74
flavkaze <flav.gaspareto@gmail.com>
2020-09-04 22:49:56 -0300

    tratando excecoes da rota delete

--- app/management/views.py
+++ app/management/views.py
@@ -11,10 +11,12 @@ class DeleteApi(MethodView):
 		"""
  		Delete a user
   		"""
-		if not isinstance(user_id, int):
+		# if not isinstance(user_id, int):
+		try:
+			user = User.get_one_user(user_id)
+			if not user:
+				return make_response(jsonify({'message': 'id nao tem'}), HTTPStatus.BAD_REQUEST.value) 
+			user.delete()
+			return make_response(jsonify({'message': 'deleted'}), HTTPStatus.OK.value)
+		except Exception as e:
 			return make_response(jsonify({'message': 'precisa de um parametro inteiro'}), HTTPStatus.BAD_REQUEST.value)
-		user = User.get_one_user(user_id)
-		if not user:
-			return make_response(jsonify({'message': 'id nao tem'}), HTTPStatus.BAD_REQUEST.value) 
-		user.delete()
-		return make_response(jsonify({'message': 'deleted'}), HTTPStatus.OK.value)
